63073e80af1c7d7ffdcf5043bca2b28db3d3c180
Kirilloao <kirilltaraturin747@gmail.com>
2023-11-12 20:17:28 +0100

    add colors

---
+++ Rainbow/Extension/UIColor + Extension.swift
@@ -0,0 +1,58 @@
+//
+//  UIColor + Extension.swift
+//  Rainbow
+//
+//  Created by Kirill Taraturin on 12.11.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    // MARK: - App Colors
+    static var blueButtonColor: UIColor {
+        UIColor(named: "blueButtonColor") ?? .clear
+    }
+    
+    static var greenButtonColor: UIColor {
+        UIColor(named: "greenButtonColor") ?? .clear
+    }
+    
+    static var pinkLabelColor: UIColor {
+        UIColor(named: "pinkLabelColor") ?? .clear
+    }
+    
+    static var purpleButtonColor: UIColor {
+        UIColor(named: "purpleButtonColor") ?? .clear
+    }
+    
+    static var redButtonColor: UIColor {
+        UIColor(named: "redButtonColor") ?? .clear
+    }
+    
+    static var grayBackgroundColor: UIColor {
+        UIColor(named: "grayBackgroundColor") ?? .clear
+    }
+    
+    // MARK: - Game Colors
+    static var blueGameColor: UIColor {
+        UIColor(named: "blueGameColor") ?? .clear
+    }
+    
+    static var greenGameColor: UIColor {
+        UIColor(named: "greenGameColor") ?? .clear
+    }
+    
+    static var purpleGameColor: UIColor {
+        UIColor(named: "purpleGameColor") ?? .clear
+    }
+    
+    static var redGameColor: UIColor {
+        UIColor(named: "redGameColor") ?? .clear
+    }
+    
+    static var yellowGameColor: UIColor {
+        UIColor(named: "yellowGameColor") ?? .clear
+    }
+}
+
