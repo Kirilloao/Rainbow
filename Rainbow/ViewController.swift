f36c2689f659a6093ae4c535b39bd014aff5585d
Kirilloao <kirilltaraturin747@gmail.com>
2023-11-12 17:32:14 +0100

    initial commit

---
+++ Rainbow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rainbow
+//
+//  Created by Kirill Taraturin on 12.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
